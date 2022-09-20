5960df5fe9922010ec7d2c49a4ee1e8aea183e4e
NSchatz <noah.lane.schatz@gmail.com>
2022-09-20 18:16:02 -0400

    setup for digital ocean

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
+const path = require('path')
 const express = require("express")
 const colors = require('colors')
 const dotenv = require("dotenv").config()
 const { errorHandler } = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
 const port = process.env.PORT || 5000
+
 
 connectDB()
 
@@ -15,5 +17,13 @@ app.use(errorHandler)
 
 app.use('/api/times', require('./routes/timeRoutes'))
 app.use('/api/users', require('./routes/userRoutes'))
+
+//Serve frontend
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, '../frontend/build')))
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')))
+} else {
+    app.get('/', (req,res) => res.send('Please set to production'))
+}
 
 app.listen(port, () => console.log(`Server started on port ${port}`))
